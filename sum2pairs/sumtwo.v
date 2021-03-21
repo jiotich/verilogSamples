95dd8c9f74df6cf71cbf8f20b6de0114e0984b60
João Francisco Olivetti <jiotich@gmail.com>
2021-03-21 19:26:09 -0300

    Update sumtwo.v

--- sum2pairs/sumtwo.v
+++ sum2pairs/sumtwo.v
@@ -4,15 +4,15 @@ module sumbi(ft,st,res);
 	output wire [2:0] res;
 	
 	assign res[2] = (ft[1] & ft[0] & st[1] & st[0]) 
-					| ((ft[1] & ft[0] & st[1] & ~st[0]) | (ft[1] & ~ft[0] & st[1] & st[0]))
-					| ((ft[1] & ft[0] & ~st[1] & st[0]) | (ft[1] & ~ft[0] & st[1] & ~st[0]) | (~ft[1] & ft[0] & st[1] & st[0]));
+			| ((ft[1] & ft[0] & st[1] & ~st[0]) | (ft[1] & ~ft[0] & st[1] & st[0]))
+			| ((ft[1] & ft[0] & ~st[1] & st[0]) | (ft[1] & ~ft[0] & st[1] & ~st[0]) | (~ft[1] & ft[0] & st[1] & st[0]));
 	
 	assign res[1] = (ft[1] & ft[0] & st[1] & st[0]) 
-					| ((ft[1] & ft[0] & ~st[1] & ~st[0]) | (ft[1] & ~ft[0] & ~st[1] & st[0]) | (~ft[1] & ft[0] & st[1] & ~st[0]) | (~ft[1] & ~ft[0] & st[1] & st[0]))
-					| ((ft[1] & ~ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ft[0] & ~st[1] & st[0]) | (~ft[1] & ~ft[0] & st[1] & ~st[0]));
+			| ((ft[1] & ft[0] & ~st[1] & ~st[0]) | (ft[1] & ~ft[0] & ~st[1] & st[0]) | (~ft[1] & ft[0] & st[1] & ~st[0]) | (~ft[1] & ~ft[0] & st[1] & st[0]))
+			| ((ft[1] & ~ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ft[0] & ~st[1] & st[0]) | (~ft[1] & ~ft[0] & st[1] & ~st[0]));
 
 	assign res[0] = ((ft[1] & ft[0] & ~st[1] & ~st[0]) | (ft[1] & ~ft[0] & ~st[1] & st[0]) | (~ft[1] & ft[0] & st[1] & ~st[0]) | (~ft[1] & ~ft[0] & st[1] & st[0]))
-					| ((ft[1] & ~ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ft[0] & ~st[1] & st[0]) | (~ft[1] & ~ft[0] & st[1] & ~st[0]))
-					| ((ft[1] & ft[0] & st[1] & ~st[0]) | (ft[1] & ~ft[0] & st[1] & st[0]))
-					| ((~ft[1] & ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ~ft[0] & ~st[1] & st[0]));
+			| ((ft[1] & ~ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ft[0] & ~st[1] & st[0]) | (~ft[1] & ~ft[0] & st[1] & ~st[0]))
+			| ((ft[1] & ft[0] & st[1] & ~st[0]) | (ft[1] & ~ft[0] & st[1] & st[0]))
+			| ((~ft[1] & ft[0] & ~st[1] & ~st[0]) | (~ft[1] & ~ft[0] & ~st[1] & st[0]));
 endmodule
